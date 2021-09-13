114ed8cc6d9040dc37ed52ed38982d99e32c522d
Sonika <sonikapoudyal5@gmail.com>
2021-09-13 12:13:40 -0400

    installed apollo-client

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,65 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($bookId: savedBook!){
+    saveBook(bookId: $bookId) {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+            bookId
+            authors
+            description
+            image
+            link
+            title
+        }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!){
+    saveBook(bookId: $bookId) {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+            bookId
+            authors
+            description
+            image
+            link
+            title
+        }
+    }
+  }
+`;
+
+
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -53,7 +53,7 @@ const resolvers = {
             throw new AuthenticationError('You need to be logged in!')
         },
 
-        deleteBook: async (parent, { bookId }, context) => {
+        removeBook: async (parent, { bookId }, context) => {
             if (context.user) {
                 const updatedUser = await User.findByIdAndUpdate(
                     { _id: context.user._id },
